742cb3a14d61bfec99e9447af96e0e4649810007
FernandoNoleto <fernando.barroso.noleto@gmail.com>
2021-10-22 19:49:04 -0300

    Commit inicial

---
+++ android/app/src/main/kotlin/com/example/controle_engenharia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.controle_engenharia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:controle_engenharia/corposdeprovascreen.dart';
+
+void main() {
+  runApp(Nav2App());
+}
+
+class Nav2App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Controle Engenharia'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(size: 72),
+                title: Text("Corpos de prova"),
+                subtitle: Text("calcular fck, desvio padrão..."),
+                isThreeLine: true,
+                trailing: Icon(Icons.expand_more_outlined),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) {
+                      return CorposDeProvaScreen();
+                    }),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
